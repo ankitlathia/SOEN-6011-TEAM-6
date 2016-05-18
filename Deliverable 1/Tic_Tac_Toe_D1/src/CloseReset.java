5c84479194288fd47ff73921df96b725ecdd9e73
Raza Khan <l080221@gmail.com>
2016-05-17 21:37:34 -0400

    Add Additional Game Logic and Validations

---
+++ Deliverable 1/Tic_Tac_Toe_D1/src/CloseReset.java
@@ -0,0 +1,64 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class CloseReset extends JPanel {
+
+private JButton Exit, Reset, Help;
+public static JButton New;
+private XOButton Brd;
+
+
+
+CloseReset() {
+
+    setLayout(new FlowLayout());
+
+    Exit = new JButton("Exit");
+    Reset = new JButton("Reset");
+    Help = new JButton("Help");
+    New = new JButton("New Game");
+
+   
+
+    Exit.addActionListener(new ActionListener() {
+
+        public void actionPerformed(ActionEvent ae) {
+            System.exit(0);
+        }
+    });
+
+    Reset.addActionListener(new ActionListener() {
+
+        public void actionPerformed(ActionEvent ae) {
+        	
+            Brd.ResetGame();
+
+        }
+
+    });
+    
+    Help.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent ae) {
+        	 JOptionPane.showMessageDialog(null, "INSTRUCTIONS : \n It consist two players, X and O, \n"+
+        " who take turns marking the spaces in a 3×3 grid. \n"+
+        "The player who succeeds in placing three of their marks in a horizontal, \n vertical, or diagonal row wins the game.");
+        }
+    });
+    
+    
+   add(New);
+    add(Reset);
+    add(Help);
+    add(Exit);
+    
+}
+
+public void SetObjectOfBoard(XOButton B) {
+
+    Brd = B;
+
+}
+
+}
---
+++ Deliverable 1/Tic_Tac_Toe_D1/src/GameArray.java
@@ -0,0 +1,86 @@
+import java.awt.Color;
+
+import javax.swing.*;
+
+public class GameArray {
+
+/*
+Class for checking Win condition checking
+*/
+private XOButton Brd;
+private int GArray[];
+
+GameArray(XOButton B) {
+
+    GArray = new int[9];
+
+    Brd = B;
+
+    for (int i = 0; i < 9; i++) {
+            GArray[i] = 0;
+    }
+
+}
+
+public void ArrayInitialize(int i, int Marker) {
+
+    GArray[i] = Marker;
+    CheckValidationTOWin(Marker);
+}
+
+
+public void CheckValidationTOWin(int Marker) {
+
+  //Check horizontal boxes in board
+
+   if((GArray[0] == Marker && GArray[1] == Marker && GArray[2] == Marker) || (GArray[3] == Marker && GArray[4] == Marker && GArray[5] == Marker) || (GArray[6] == Marker && GArray[7]== Marker && GArray[8] == Marker))
+    {
+
+        if (Marker == 1) {
+        	
+            JOptionPane.showMessageDialog(Brd, "CONGRATULATIONS : "+ TicTacToe.player.get(0) +" Win the game");
+
+        } else if (Marker == 2) {
+
+            JOptionPane.showMessageDialog(Brd, "CONGRATULATIONS : "+ TicTacToe.player.get(1) +" Win the game");
+
+        }
+
+        Brd.DisableAllPositions(false);
+
+    } else 
+    	
+    	if ((GArray[0] == Marker && GArray[3] == Marker && GArray[6] == Marker) || (GArray[1] == Marker && GArray[4] == Marker && GArray[7] == Marker) || (GArray[2] == Marker && GArray[5] == Marker && GArray[8] == Marker))	
+    {
+    		//Check vertical boxes in board
+    		
+        if (Marker == 1) {
+            JOptionPane.showMessageDialog(Brd, "CONGRATULATIONS : "+ TicTacToe.player.get(0) +" Win the game");
+
+        } else if (Marker == 2) {
+            JOptionPane.showMessageDialog(Brd, "CONGRATULATIONS : "+ TicTacToe.player.get(1) +" Win the game");
+
+        }
+
+        Brd.DisableAllPositions(false);
+
+    } else 
+    	if ((GArray[0] == Marker && GArray[4] == Marker && GArray[8] == Marker) || (GArray[6] == Marker && GArray[4] == Marker && GArray[2] == Marker)) 
+    	
+    	{
+    		//Check diagonal boxes in board
+    		
+        if (Marker == 1) {
+            JOptionPane.showMessageDialog(Brd, "CONGRATULATIONS : "+ TicTacToe.player.get(0) +" Win the game");
+
+        } else if (Marker == 2) {
+            JOptionPane.showMessageDialog(Brd, "CONGRATULATIONS : "+ TicTacToe.player.get(1) +" Win the game");
+
+        }
+        Brd.DisableAllPositions(false);
+
+    }
+
+}
+
+}
